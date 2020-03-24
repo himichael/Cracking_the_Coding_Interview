92a5cae994fb3da17ea97fbbc65ed28345b5cc6d
yang.wang09 <yang.wang09@ucarinc.com>
2020-03-24 10:28:00 +0800

    增加一题，按摩师

---
+++ 17.16.按摩师/the-masseuse-lcci.py
@@ -0,0 +1,60 @@
+﻿class Solution(object):
+	# 递归(超时)
+	def massage(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		if not nums:
+			return 0
+		n = len(nums)
+		def dfs(index,status):
+			if index==n:
+				return 0
+			a,b,c = 0,0,0
+			a = dfs(index+1,status)
+			if status:
+				b = dfs(index+1,False)+nums[index]
+			else:
+				c = dfs(index+1,True)
+			return max(a,b,c)
+		return dfs(0,True)
+		
+		
+
+	# 动态规划，一维数组
+	def massage(self, nums):
+		if not nums or len(nums)==0:
+			return 0
+		if len(nums)==1:
+			return nums[0]
+		n = len(nums)
+		dp = [0 for _ in xrange(n)]
+		dp[0] = nums[0]
+		dp[1] = max(nums[0],nums[1])
+		for i in xrange(2,n):
+			dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+		return dp[-1]
+		
+		
+		
+	# 动态规划，O(1)空间
+	def massage(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		if not nums or len(nums)==0:
+			return 0
+		if len(nums)==1:
+			return nums[0]
+		n = len(nums)
+		dp0 = nums[0]
+		dp1 = max(nums[0],nums[1])
+		for i in xrange(2,n):
+			tmp = max(dp0+nums[i],dp1)
+			dp0 = dp1
+			dp1 = tmp
+		return max(dp0,dp1)
+		
+		
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 |---| --- | --- | --- | --- | --- | --- | --- |
 |面试题 01.06|[字符串压缩](https://leetcode-cn.com/problems/compress-string-lcci/)|c++|java|[python](./01.06.字符串压缩/compress-string-lcci.py)|go||Easy|
 |面试题 10.01|[合并排序的数组](https://leetcode-cn.com/problems/sorted-merge-lcci/)|c++|java|[python](./10.01.合并排序的数组/sorted-merge-lcci.py)|go||Easy|
+|面试题 17.16|[按摩师](https://leetcode-cn.com/problems/the-masseuse-lcci/)|c++|java|[python](./17.16.按摩师/the-masseuse-lcci.py)|go||Easy|
 
 
 
