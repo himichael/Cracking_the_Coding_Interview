29115df52d3059dae4d53668b2693e2fcb8c7222
michaelwng <michaelwangg@qq.com>
2020-06-26 11:52:19 +0800

    增加一题，移除重复节点

---
+++ 02.01.移除重复节点/remove-duplicate-node-lcci.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+	def removeDuplicateNodes(self, head):			
+		if not (head and head.next):
+			return head
+		s = set()
+		s.add(head.val)
+		p = head
+		while p and p.next:
+			next = p.next
+			if next.val not in s:
+				s.add(next.val)
+				p = p.next
+			else:
+				p.next = next.next
+		return head
