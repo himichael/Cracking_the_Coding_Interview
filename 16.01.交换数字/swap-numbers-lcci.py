04a41755f63d1c51bccdc91dd50a610e23cf2511
micahel <michaelwangg@qq.com>
2020-07-01 13:49:06 +0800

    增加16.01的实现

--- 16.01.交换数字/swap-numbers-lcci.py
+++ 16.01.交换数字/swap-numbers-lcci.py
@@ -8,3 +8,21 @@
             return []
         numbers[0],numbers[1] = numbers[1],numbers[0]
         return numbers
+		
+		
+		
+	# 使用加减法	
+    def swapNumbers(self, numbers):
+        numbers[0] = numbers[0]+numbers[1]
+        numbers[1] = numbers[0]-numbers[1]
+        numbers[0] = numbers[0]-numbers[1]
+        return numbers
+		
+		
+		
+	# 使用异或运算
+    def swapNumbers(self, numbers):
+        numbers[0] ^= numbers[1]
+        numbers[1] ^= numbers[0]
+        numbers[0] ^= numbers[1]
+        return numbers
