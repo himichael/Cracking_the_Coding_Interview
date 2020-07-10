4f95b350386e4f47b799ba228b02e7d9724daaad
michaelwng <michaelwangg@qq.com>
2020-07-10 10:57:07 +0800

    增加两题

---
+++ 16.11.跳水板/diving-board-lcci.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+    def divingBoard(self, shorter, longer, k):
+        """
+        :type shorter: int
+        :type longer: int
+        :type k: int
+        :rtype: List[int]
+        """
+        if k==0:
+            return []
+        if shorter==longer:
+            return [k*shorter]
+        dp = [0 for _ in xrange(k+1)]
+        for i in xrange(k+1):
+            dp[i] = shorter*(k-i) + longer*i
+        return dp
