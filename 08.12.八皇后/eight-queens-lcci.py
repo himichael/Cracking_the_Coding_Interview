8cf24e9d68ca9ea97eec6f711d8a47da4866f5e4
michaelwng <michaelwangg@qq.com>
2020-09-06 13:02:55 +0800

    增加八皇后

---
+++ 08.12.八皇后/eight-queens-lcci.py
@@ -0,0 +1,42 @@
+﻿class Solution(object):
+    def solveNQueens(self, n):
+        # 创建一个N行的数组，下标i对应N*N棋盘格子第i行的皇后位置
+        arr = [-1 for _ in xrange(n)]
+        # 三个集合，分别判断某一列，左斜线(左上到右下的斜线)，右斜线(左下到右上的斜线)
+        columns = set()
+        hypotenuse1 = set()
+        hypotenuse2 = set()
+        res = []
+        def dfs(x):
+            if x==n:
+                # 如果x==n说明所有的皇后都摆放完了
+                # 将arr数组中保存的结果拼接起来
+                tmp = []
+                row = ["."]*n
+                for k in xrange(n):
+                    row[arr[k]] = "Q"
+                    tmp.append("".join(row))
+                    row[arr[k]] = "."
+                res.append(tmp)
+                return
+            # 遍历一行中的每一列，并检查竖线、左斜线、右斜线是否有皇后    
+            for y in xrange(n):
+                if y in columns:
+                    continue
+                if x-y in hypotenuse1:
+                    continue
+                if x+y in hypotenuse2:
+                    continue
+                # 如果检查通过，设置这一行的皇后位置，并将竖线、左斜线、右斜线的值放入集合中，并继续下一行递归    
+                # 当下一层的所有递归遍历完后，回到本轮需要将之前集合、arr数组中保存的结果都清空
+                arr[x] = y
+                columns.add(y)
+                hypotenuse1.add(x-y)
+                hypotenuse2.add(x+y)
+                dfs(x+1)
+                arr[x] = -1
+                columns.remove(y)
+                hypotenuse1.remove(x-y)
+                hypotenuse2.remove(x+y)
+        dfs(0)
+        return res
--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 |面试题 08.05|[递归乘法](https://leetcode-cn.com/problems/recursive-mulitply-lcci/)|c++|java|[python](./08.05.递归乘法/recursive-mulitply-lcci.py)|go||Medium|
 |面试题 08.09|[括号](https://leetcode-cn.com/problems/bracket-lcci/)|c++|java|[python](./08.09.括号/bracket-lcci.py)|go||Medium|
 |面试题 08.11|[硬币](https://leetcode-cn.com/problems/coin-lcci/)|c++|java|[python](./08.11.硬币/coin-lcci.py)|go||Medium|
+|面试题 08.12|[八皇后](https://leetcode-cn.com/problems/eight-queens-lcci/)|c++|java|[python](./08.12.八皇后/eight-queens-lcci.py.py)|go||Hard|
 |面试题 10.01|[合并排序的数组](https://leetcode-cn.com/problems/sorted-merge-lcci/)|c++|java|[python](./10.01.合并排序的数组/sorted-merge-lcci.py)|go||Easy|
 |面试题 16.01|[交换数字](https://leetcode-cn.com/problems/swap-numbers-lcci/)|c++|java|[python](./16.01.交换数字/swap-numbers-lcci.py)|go||Medium|
 |面试题 16.03|[交点](https://leetcode-cn.com/problems/intersection-lcci/)|c++|java|[python](./16.03.交点/intersection-lcci.py)|go||Easy|
@@ -14,6 +15,8 @@
 |面试题 16.18|[模式匹配](https://leetcode-cn.com/problems/pattern-matching-lcci/)|c++|java|[python](./16.18.模式匹配/pattern-matching-lcci.py)|go||Medium|
 |面试题 17.13|[恢复空格](https://leetcode-cn.com/problems/re-space-lcci/)|c++|[java](./17.13.恢复空格/re-space-lcc.java)|python|go||Medium|
 |面试题 17.16|[按摩师](https://leetcode-cn.com/problems/the-masseuse-lcci/)|c++|java|[python](./17.16.按摩师/the-masseuse-lcci.py)|go||Easy|
+
+
 
 
 
